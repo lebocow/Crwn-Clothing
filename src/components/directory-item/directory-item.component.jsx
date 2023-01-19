5b89a47202b5bac80755704fb475424060872ece
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2023-01-19 16:34:55 +0200

    Implementing directory item redirect + removed some console logs

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   BackgroundImage,
   Body,
@@ -5,8 +7,11 @@ import {
 } from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({ category: { imageUrl, title } }) => {
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(`/shop/${title}`);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
