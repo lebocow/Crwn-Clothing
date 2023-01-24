5a4408f5798d34fe15c934680002ce6a4e3c4955
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2023-01-24 11:27:13 +0200

    Adding redux-persist + composeEnhancer for redux devtools extension chrome

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -18,7 +18,6 @@ import { selectIsCartOpen } from "../../store/cart/cart.selector";
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
-  console.log(isCartOpen);
 
   return (
     <Fragment>
